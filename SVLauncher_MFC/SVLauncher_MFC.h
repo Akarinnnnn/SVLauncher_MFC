ebb1dbfa2493bb82e3155d6c90681fe56cde55b9
Akarinnnnn <1207247008@qq.com>
2018-12-15 23:39:42 +0800

    起步

---
+++ SVLauncher_MFC/SVLauncher_MFC.h
@@ -0,0 +1,32 @@
+﻿
+// SVLauncher_MFC.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// SVLauncherMFC_App:
+// 有关此类的实现，请参阅 SVLauncher_MFC.cpp
+//
+
+class SVLauncherMFC_App : public CWinApp
+{
+public:
+	SVLauncherMFC_App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern SVLauncherMFC_App theApp;
---
+++ SVLauncher_MFC/resource.h
@@ -0,0 +1,30 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 SVLauncherMFC.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_SVLAUNCHER_MFC_DIALOG       102
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT1                       1003
+#define IDC_RS                          1003
+#define IDC_CONFDIR                     1004
+#define IDC_CLUSTER                     1005
+#define IDC_SHARD                       1006
+#define IDC_S_ID64                      1007
+#define IDC_S_ID3                       1008
+#define IDC_LOG                         1009
+#define IDC_LOCALSV                     1010
+#define IDC_FO                          1011
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
